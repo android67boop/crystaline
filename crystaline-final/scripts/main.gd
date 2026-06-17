3fc2f1d2b4c8f35ae703bc9ff974ac1c7dd8b31e
android67boop <arundhotidan07@gmail.com>
2026-06-17 13:30:25 +1200

    player movement

---
+++ crystaline-final/scripts/main.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _play() -> void:
+	get_tree().call_deferred("change_scene_to_astronaut", "res://scenes/main.tscn")
+
+
+
+func _quit() -> void:
+	get_tree().quit()
+
+	
+
+
+func _on_button_pressed() -> void:
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/astronaut.tscn")
+	
+	
+
+
+func _on_button_2_pressed() -> void:
+	pass  #Replace with function body.
+
+
+func _on_button_3_pressed() -> void:
+	pass # Replace with function body.
+	
+	
+	
+	
