d1382025e39014997eb4acba73f3c4a9c114c33d
android67boop <arundhotidan07@gmail.com>
2026-06-25 15:10:13 +1200

    made player walk

--- crystaline-final/scripts/main.gd
+++ crystaline-final/scripts/main.gd
@@ -23,7 +23,7 @@ func _quit() -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().call_deferred("change_scene_to_file", "res://scenes/astronaut.tscn")
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/planet1.tscn")
 	
 	
 
