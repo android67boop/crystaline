6af86e42c81a51bd0a9cb5b7c7dd132909656c1b
android67boop <arundhotidan07@gmail.com>
2026-07-21 09:26:30 +1200

    made alien

--- crystaline-final/scripts/alien.gd
+++ crystaline-final/scripts/alien.gd
@@ -1,11 +1,17 @@
-extends Node2D
+extends CharacterBody2D
 
+var speed = 60
+var direction = -1
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _physics_process(delta):
+	
+	if !$WallRay.is_colliding() or !$EdgeRay.is_colliding():
+		
+		direction *= -1
+		
+		scale.x *= -1
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	velocity.x = speed * direction
+	
+	
+	move_and_slide()
